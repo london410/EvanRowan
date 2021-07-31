f7e760041629a199c342b115a69cc4957e7de701
london410 <78399781+london410@users.noreply.github.com>
2021-08-01 03:29:25 +0600

    Create Evansp.py
    https://github.com/london410/EvanRowan.git
    ls
    Cd EvanRowan
    ls
    Python2 Evansp.py

---
+++ Evansp.py
@@ -0,0 +1,13 @@
+echo "# EvanRowan" >> README.md
+
+git init
+
+git add README.md
+
+git commit -m "first commit"
+
+git branch -M main
+
+git remote add origin https://github.com/london410/EvanRowan.git
+
+git push -u origin main
